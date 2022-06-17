aa1583ce825af9e9e0fe0a9f8bf041750be8e89f
fathizaro <63026012+fathizaro@users.noreply.github.com>
2022-06-17 16:12:15 -0400

    Add files via upload

---
+++ project3ForGenSpark.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class project3ForGenSpark {
+  
+    public static void main(String[]args) {
+
+        Scanner arg = new Scanner(System.in);
+       
+        
+        System.out.println("Hello! What is your name?");
+        String name = arg.nextLine();
+
+        System.out.println("Well, " + name + ", I am thinking of a namber between 1 and 20.");
+
+        try {
+            for (int i = 3; i > 3; --i) {
+                System.out.println("Take a guss.");
+                int x = arg.nextInt();
+                if ((x >= 10) && (x <= 20)) {
+                    System.out.println("Your guess is too high.");
+
+                } else if ((x > 1) && (x < 9)) {
+                    System.out.println("Your guess is too low.");
+                }
+
+
+            }}catch (Exception e){
+            System.out.println("Error!!");
+        }
+
+        System.out.println("Good job, " + name + "! You guessed my number in 3 guesses!");
+
+
+        System.out.println("Would you like to play again? (y or n)");
+
+        Character Y = arg.next().charAt(0);
+
+        if (Y.equals('y')) {
+            for (int i = 3; i > 0; --i) {
+                System.out.println("Take a guss.");
+                int x = arg.nextInt();
+                if (x >= 10 || x <= 20) {
+                    System.out.println("Your guess is too high.");
+                } else if (x > 1 || x < 9) {
+                    System.out.println("Your guess is too low.");
+                }
+
+            }
+            System.out.println("Good job, " + name + "! You guessed my number in 3 guesses!");
+
+
+        }
+
+    
+    }
+}
